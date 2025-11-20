ae2538f3af1230599382fe3e638b8475d45e570f
Andrick Avila Villaseñor <avilaandrick8@gmail.com>
2025-11-20 07:48:25 -0600

    Se agrego un inventario a los pacientes, desde la clase abstracta persona

--- Persona.java
+++ Persona.java
@@ -1,8 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class Persona {
     protected String nombre_persona;
     protected String direccion;
     protected int edad;
     protected long numero_telefono;
+    protected List <Objeto> inventario;
 
     //Constructor
     Persona(String nombre_persona, String direccion, int edad, long numero_telefono){
@@ -10,6 +14,8 @@ public abstract class Persona {
         this.direccion = direccion;
         this.edad = edad;
         this.numero_telefono = numero_telefono;
+        //Iniciando el inventario de la persona
+        this.inventario = new ArrayList<>();
     }
 
     //Getters y Setters
@@ -51,5 +57,11 @@ public abstract class Persona {
         System.out.println("Direccion: " + direccion);
         System.out.println("Edad: " + edad);
         System.out.println("Numero de telefono: " + numero_telefono);
+    }
+
+    public void bienvenida(){
+    }
+
+    public void ver_inventario(){
     }
 }
